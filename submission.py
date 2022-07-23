a85fe4cc146ec0ef54bcf860fcd0137b75422cfe
apo <tagliabue.jacopo@gmail.com>
2022-07-23 22:38:22 +0200

    simplify submission code by exporting some vars to envs

--- submission.py
+++ submission.py
@@ -13,21 +13,28 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 # import env variables from file
-load_dotenv('../upload.env', verbose=True)
+load_dotenv('upload.env', verbose=True)
 
 
 EMAIL = os.getenv('EMAIL')  # the e-mail you used to sign up
+assert EMAIL != '' and EMAIL is not None
 BUCKET_NAME = os.getenv('BUCKET_NAME')  # you received it in your e-mail
 PARTICIPANT_ID = os.getenv('PARTICIPANT_ID')  # you received it in your e-mail
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')  # you received it in your e-mail
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')  # you received it in your e-mail
+UPLOAD = bool(os.getenv('UPLOAD'))  # it's a boolean, True if you want to upload your submission
+LIMIT = int(os.getenv('LIMIT'))  # limit the number of test cases, for quick tests / iterations. 0 for no limit
+
+print("Submission will be uploaded: {}".format(UPLOAD))
+if LIMIT > 0:
+    print("WARNING: only {} test cases will be used".format(LIMIT))
 
 
 # run the evaluation loop when the script is called directly
 if __name__ == '__main__':
     # import YOUR runner class, which is an instance of the general EvalRSRunner class
     from submission.my_runner import MyEvalRSRunner
-    print('==== Starting evaluation script at: {} ====\n'.format(datetime.utcnow()))
+    print('\n\n==== Starting evaluation script at: {} ====\n'.format(datetime.utcnow()))
     # run the evaluation loop
     runner = MyEvalRSRunner(
         num_folds=4,
@@ -38,5 +45,5 @@ if __name__ == '__main__':
         email=EMAIL
         )
     print('==== Runner loaded, starting loop at: {} ====\n'.format(datetime.utcnow()))
-    runner.evaluate(upload=True, limit=1000000)
+    runner.evaluate(upload=UPLOAD, limit=LIMIT)
     print('\n\n==== Evaluation ended at: {} ===='.format(datetime.utcnow()))
