d58f8d84f91d3eeb1acc490da5dbb520bd64d5ea
Federico Bianchi <chiccobia@gmail.com>
2022-09-02 12:17:21 -0700

    Update EvalRSRunner.py

--- evaluation/EvalRSRunner.py
+++ evaluation/EvalRSRunner.py
@@ -202,7 +202,7 @@ class EvalRSRunner:
         self._folds = None
         self.model = None
         self.dataset = dataset
-        self.secret = 1214210679
+        self.secret = 4
 
         resp = requests.get(url="https://raw.githubusercontent.com/RecList/evalRS-CIKM-2022/main/secret.json")
         data = resp.json()
