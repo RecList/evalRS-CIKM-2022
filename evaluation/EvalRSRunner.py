82d22a4750badd748e6bf038ca2a4f137ffe9cba
vinid <chiccobia@gmail.com>
2022-09-02 19:37:15 -0700

    type

--- evaluation/EvalRSRunner.py
+++ evaluation/EvalRSRunner.py
@@ -378,8 +378,8 @@ class EvalRSRunner:
                                   participant_id=self.participant_id,
                                   bucket_name=self.bucket_name)
         if debug:
-            time_for_submissison = (time.time() - start)/60
-            print(f"Entire Process Duration: {time_for_submissison} Minutes")
+            time_for_submission = (time.time() - start)/60
+            print(f"Entire Process Duration: {time_for_submission} Minutes")
 
     def __hash__(self):
         hash_inputs = [
